5d81fdf8ed1c292aa60b6cb48ba9e555c2cd9f5a
DeadSith <deadsith@hotmail.com>
2018-05-26 20:48:07 +0300

    Added default controllers

---
+++ src/Lightest.Api/Controllers/LanguagesController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lightest.Data;
+using Lightest.Data.Models;
+
+namespace Lightest.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class LanguagesController : Controller
+    {
+        private readonly RelationalDbContext _context;
+
+        public LanguagesController(RelationalDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Languages
+        [HttpGet]
+        public IEnumerable<Language> GetLanguages()
+        {
+            return _context.Languages;
+        }
+
+        // GET: api/Languages/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLanguage([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var language = await _context.Languages.FindAsync(id);
+
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(language);
+        }
+
+        // PUT: api/Languages/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLanguage([FromRoute] int id, [FromBody] Language language)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != language.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(language).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LanguageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Languages
+        [HttpPost]
+        public async Task<IActionResult> PostLanguage([FromBody] Language language)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Languages.Add(language);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLanguage", new { id = language.Id }, language);
+        }
+
+        // DELETE: api/Languages/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLanguage([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var language = await _context.Languages.FindAsync(id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            _context.Languages.Remove(language);
+            await _context.SaveChangesAsync();
+
+            return Ok(language);
+        }
+
+        private bool LanguageExists(int id)
+        {
+            return _context.Languages.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ src/Lightest.Api/Controllers/TasksController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lightest.Data;
+using Lightest.Data.Models;
+
+namespace Lightest.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class TasksController : Controller
+    {
+        private readonly RelationalDbContext _context;
+
+        public TasksController(RelationalDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tasks
+        [HttpGet]
+        public IEnumerable<Data.Models.Task> GetTasks()
+        {
+            return _context.Tasks;
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTask([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var task = await _context.Tasks.FindAsync(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        // PUT: api/Tasks/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask([FromRoute] int id, [FromBody] Data.Models.Task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != task.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tasks
+        [HttpPost]
+        public async Task<IActionResult> PostTask([FromBody] Data.Models.Task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTask", new { id = task.Id }, task);
+        }
+
+        // DELETE: api/Tasks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return Ok(task);
+        }
+
+        private bool TaskExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
+    }
+}
