a4986ec8417d5a9638ad4f01d56189af83de40b6
Andrii Vasylyk <deadsith@hotmail.com>
2019-08-10 22:14:45 +0300

    Moved tests to single project
    Enabled coverage uploading

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 dotnet build
-dotnet test
+dotnet test /p:CollectCoverage=true /p:CoverletOutput=../../../cover.xml /p:CoverletOutputFormat=opencover
 dotnet tool install coveralls.net --version 1.0.0 --tool-path tools
-.\tools\csmacnz.Coveralls 
+./tools/csmacnz.Coveralls --opencover -i cover.xml --useRelativePaths
