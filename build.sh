3c91609ca2801d14dfd7f45769f095c9eeaf7b58
Andrii Vasylyk <deadsith@hotmail.com>
2019-08-11 21:39:45 +0300

    Fixed code coverage generation
    Excluded unnecessary types, added metadata to uploader

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 dotnet build
-dotnet test /p:CollectCoverage=true /p:CoverletOutput=../../../cover.xml /p:CoverletOutputFormat=opencover
+dotnet test @coverage.rsp
 dotnet tool install coveralls.net --version 1.0.0 --tool-path tools
-./tools/csmacnz.Coveralls --opencover -i cover.xml --useRelativePaths
+./tools/csmacnz.Coveralls --opencover -i cover.xml --useRelativePaths \
+--commitId $TRAVIS_COMMIT --commitBranch $TRAVIS_BRANCH --commitAuthor \
+$AUTHOR_NAME --commitEmail $COMMITTER_EMAIL --commitMessage $TRAVIS_COMMIT_MESSAGE \
+--jobId $TRAVIS_BUILD_NUMBER --serviceName travis
