ae8b2fdb9b28c481689d89aed730520339c23ed5
Andrii Vasylyk <deadsith@hotmail.com>
2019-11-02 15:20:57 +0200

    Fix coverage uploading

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+set -e
 set -v
 
 dotnet build
@@ -6,12 +7,9 @@ dotnet build
 # they are moved to custrom .rsp file
 dotnet test @coverage.rsp
 
-# upload coverage
-dotnet tool install coveralls.net --version 1.0.0 --tool-path tools
-./tools/csmacnz.Coveralls --opencover -i cover.xml --useRelativePaths \
---commitId $TRAVIS_COMMIT --commitBranch "$TRAVIS_BRANCH" \
---commitAuthor "$AUTHOR_NAME" --commitEmail "$COMMITTER_EMAIL" \
---commitMessage "$TRAVIS_COMMIT_MESSAGE" \
---jobId "$TRAVIS_JOB_ID" --serviceName travis
+# Upload coverage data
+curl -s https://codecov.io/bash > codecov
+chmod +x codecov
+./codecov -f "cover.xml" -t $CODECOV_TOKEN
 
 set +v
