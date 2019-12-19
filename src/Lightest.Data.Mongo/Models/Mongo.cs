c32eb12c7f2b400bc75065777923f9ece8d59e03
Sushytska <valia.sushytska@gmail.com>
2019-12-19 23:03:28 +0200

    Creted new project Lightest.Data.Mongo

---
+++ src/Lightest.Data.Mongo/Models/Mongo.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Lightest.Data.Mongo.Models
+{
+    public class Mongo
+    {
+        public Guid Id { get; set; }
+        public long Code { get; set; }
+        
+    }
+}
