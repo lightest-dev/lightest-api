33ab6ae07c9af52c5b0028e6b4c587692a06484f
Sushytska <valia.sushytska@gmail.com>
2020-01-14 12:14:57 +0200

    Created class with MongoDB settings

---
+++ src/Lightest.Data.Mongo/Models/Options/MongoDBStoreDatabaseSettings.cs
@@ -0,0 +1,8 @@
+namespace Lightest.Data.Mongo.Models.Options
+{
+    public class MongoDBStoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
