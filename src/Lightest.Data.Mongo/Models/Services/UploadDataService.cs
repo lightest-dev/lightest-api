238a4d6518756ab711dff5cb1125646c36fc50bd
Sushytska <31610894+Sushytska@users.noreply.github.com>
2020-01-14 15:03:51 +0200

    Created repository for MongoDb (#45)

---
+++ src/Lightest.Data.Mongo/Models/Services/UploadDataService.cs
@@ -0,0 +1,36 @@
+using System;
+using Lightest.Data.Mongo.Models.Options;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Lightest.Data.Mongo.Models.Services
+{
+    public class UploadDataService
+    {
+        private readonly IMongoCollection<UploadData> _uploadData;
+        
+        protected string CollectionName => nameof(UploadData);
+
+        private readonly MongoDBStoreDatabaseSettings _mongoDBStoreDatabaseSettings;
+        
+        IMongoDatabase database;
+        public UploadDataService(IOptionsMonitor<MongoDBStoreDatabaseSettings> optionsMonitor)
+        {
+            _mongoDBStoreDatabaseSettings = optionsMonitor.CurrentValue;
+            _uploadData = database.GetCollection<UploadData>(CollectionName);
+        }
+        
+        public UploadData Get(Guid id) =>
+            _uploadData.Find<UploadData>(uploadData => uploadData.Id == id).FirstOrDefault();
+
+        public UploadData Add(UploadData uploadData)
+        {
+            _uploadData.InsertOne(uploadData);
+            return uploadData;
+        }
+        
+        public void Delete(Guid id) => 
+            _uploadData.DeleteOne(uploadData => uploadData.Id == id);
+
+    }
+}
