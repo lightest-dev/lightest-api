1652cbd00b873d670f8fa18208dc310c1e3951f9
Sushytska <valia.sushytska@gmail.com>
2020-01-19 22:07:10 +0200

    Changed class MongoServiceRegistrator on static class and added key word this

--- src/Lightest.Data.Mongo/Models/Services/MongoServiceRegistrator.cs
+++ src/Lightest.Data.Mongo/Models/Services/MongoServiceRegistrator.cs
@@ -3,9 +3,9 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Lightest.Data.Mongo.Models.Services
 {
-    public class MongoServiceRegistrator
+    public static class MongoServiceRegistrator
     {
-        public static IServiceCollection AddMongoCodeRepository(IServiceCollection services, IConfiguration config)
+        public static IServiceCollection AddMongoCodeRepository(this IServiceCollection services, IConfiguration config)
         {
             services.AddSingleton<IUploadDataRepository, UploadDataService>();
             services.Configure<UploadDataService>(config);
