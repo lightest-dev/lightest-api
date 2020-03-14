e81979b69e95a3a32309c01cfc286724dd071626
Sushytska <valia.sushytska@gmail.com>
2020-03-14 18:44:59 +0200

    Modified Lightest.Data.Mongo

--- src/Lightest.Data.Mongo/Models/Options/MongoDBStoreDatabaseSettings.cs
+++ src/Lightest.Data.Mongo/Models/Options/MongoDBStoreDatabaseSettings.cs
@@ -2,6 +2,7 @@
 {
     public class MongoDBStoreDatabaseSettings
     {
+        public string UploadDataCollectionName { get; set; }
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
     }
--- src/Lightest.Data.Mongo/Models/UploadData.cs
+++ src/Lightest.Data.Mongo/Models/UploadData.cs
@@ -1,20 +1,20 @@
 ﻿using System;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Lightest.Data.Mongo.Models
 {
+    [BsonIgnoreExtraElements]
     public class UploadData
     {
         [BsonId]
+        [BsonRepresentation(BsonType.String)]
         public Guid Id { get; set; }
 
         public string Code { get; set; }
 
         public byte[] Project { get; set; }
-
-        public UploadData()
-        {
-        }
+        public UploadData(){}
 
         public UploadData(Guid uploadId, string code)
         {
