f8903baa3ca0fd8ecbc0e8fc7f15c26e39a17ef0
Andrii Vasylyk <deadsith@hotmail.com>
2020-05-31 21:25:55 +0300

    Move task assignment to a separate controller

---
+++ src/Lightest.Api/Controllers/AssignmentsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lightest.AccessService.Interfaces;
+using Lightest.Api.ResponseModels;
+using Lightest.Api.ResponseModels.TaskViews;
+using Lightest.Data;
+using Lightest.Data.Models;
+using Lightest.Data.Models.TaskModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lightest.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("[controller]")]
+    public class AssignmentsController : BaseUserController
+    {
+        private readonly IAccessService<TaskDefinition> _accessService;
+
+        public AssignmentsController(
+            RelationalDbContext context,
+            UserManager<ApplicationUser> userManager,
+            IAccessService<TaskDefinition> accessService) : base(context, userManager)
+        {
+            _accessService = accessService;
+        }
+
+        [HttpGet("my")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<UserTaskView>))]
+        public IActionResult GetAssignedTasks()
+        {
+            var id = User.Claims.SingleOrDefault(c => c.Type == "sub").Value;
+            var assignedTasks = _context.UserTasks.AsNoTracking()
+                .Include(ut => ut.Task)
+                .Where(ut => ut.UserId == id);
+
+            return Ok(assignedTasks.Select(t => new UserTaskView
+            {
+                Id = t.TaskId,
+                Name = t.Task.Name,
+                Deadline = t.Deadline,
+                Completed = t.Completed,
+                HighScore = t.HighScore
+            }));
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<AccessRightsUser>))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetUsers([FromRoute] Guid id)
+        {
+            var task = await _context.Tasks
+                .AsNoTracking()
+                .Include(t => t.Users)
+                .ThenInclude(u => u.User)
+                .SingleOrDefaultAsync(t => t.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            if (!await _accessService.CanEdit(task.Id, await GetCurrentUser()))
+            {
+                return Forbid();
+            }
+
+            return Ok(task.Users.Select(u => new AccessRightsUser
+            {
+                Id = u.UserId,
+                UserName = u.User.UserName,
+                CanRead = u.CanRead,
+                CanWrite = u.CanWrite,
+                CanChangeAccess = u.CanChangeAccess,
+                IsOwner = u.IsOwner
+            }));
+        }
+
+        [HttpPost("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> AddUsers([FromRoute] Guid id, [FromBody] UserTask[] users)
+        {
+            var task = await _context.Tasks
+               .Include(t => t.Users)
+               .SingleOrDefaultAsync(t => t.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            if (!await _accessService.CanEdit(task.Id, await GetCurrentUser()))
+            {
+                return Forbid();
+            }
+
+            foreach (var user in users)
+            {
+                var existingUser = task.Users.SingleOrDefault(u => u.UserId == user.UserId);
+                if (existingUser == null)
+                {
+                    user.IsOwner = false;
+                    user.TaskId = id;
+                    task.Users.Add(user);
+                }
+                else
+                {
+                    if (existingUser.IsOwner)
+                    {
+                        continue;
+                    }
+                    //todo: add rights check
+                    existingUser.Deadline = user.Deadline;
+                    existingUser.CanRead = user.CanRead;
+                    existingUser.CanWrite = user.CanWrite;
+                    existingUser.CanChangeAccess = user.CanChangeAccess;
+                }
+            }
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
--- src/Lightest.Api/Controllers/TasksController.cs
+++ src/Lightest.Api/Controllers/TasksController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Lightest.AccessService.Interfaces;
-using Lightest.Api.ResponseModels;
 using Lightest.Api.ResponseModels.Checker;
 using Lightest.Api.ResponseModels.Language;
 using Lightest.Api.ResponseModels.TaskViews;
@@ -55,25 +54,6 @@ namespace Lightest.Api.Controllers
             tasks = _sieveProcessor.Apply(sieveModel, tasks);
 
             return Ok(tasks);
-        }
-
-        [HttpGet("tasks")]
-        [ProducesResponseType(200, Type = typeof(IEnumerable<UserTaskView>))]
-        public IActionResult GetAssignedTasks()
-        {
-            var id = User.Claims.SingleOrDefault(c => c.Type == "sub").Value;
-            var assignedTasks = _context.UserTasks.AsNoTracking()
-                .Include(ut => ut.Task)
-                .Where(ut => ut.UserId == id);
-
-            return Ok(assignedTasks.Select(t => new UserTaskView
-            {
-                Id = t.TaskId,
-                Name = t.Task.Name,
-                Deadline = t.Deadline,
-                Completed = t.Completed,
-                HighScore = t.HighScore
-            }));
         }
 
         // GET: api/Tasks/5
@@ -139,40 +119,6 @@ namespace Lightest.Api.Controllers
             return Ok(result);
         }
 
-        [HttpGet("{id}/users")]
-        [ProducesResponseType(200, Type = typeof(IEnumerable<AccessRightsUser>))]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(403)]
-        [ProducesResponseType(404)]
-        public async Task<IActionResult> GetUsers([FromRoute] Guid id)
-        {
-            var task = await _context.Tasks
-                .AsNoTracking()
-                .Include(t => t.Users)
-                .ThenInclude(u => u.User)
-                .SingleOrDefaultAsync(t => t.Id == id);
-
-            if (task == null)
-            {
-                return NotFound();
-            }
-
-            if (!await _accessService.CanEdit(task.Id, await GetCurrentUser()))
-            {
-                return Forbid();
-            }
-
-            return Ok(task.Users.Select(u => new AccessRightsUser
-            {
-                Id = u.UserId,
-                UserName = u.User.UserName,
-                CanRead = u.CanRead,
-                CanWrite = u.CanWrite,
-                CanChangeAccess = u.CanChangeAccess,
-                IsOwner = u.IsOwner
-            }));
-        }
-
         // POST: api/Tasks
         [HttpPost]
         [ProducesResponseType(typeof(TaskDefinition), 201)]
@@ -210,52 +156,6 @@ namespace Lightest.Api.Controllers
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetTask", new { id = task.Id }, task);
-        }
-
-        [HttpPost("{id}/users")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(403)]
-        [ProducesResponseType(404)]
-        public async Task<IActionResult> AddUsers([FromRoute] Guid id, [FromBody] UserTask[] users)
-        {
-            var task = await _context.Tasks
-               .Include(t => t.Users)
-               .SingleOrDefaultAsync(t => t.Id == id);
-
-            if (task == null)
-            {
-                return NotFound();
-            }
-
-            if (!await _accessService.CanEdit(task.Id, await GetCurrentUser()))
-            {
-                return Forbid();
-            }
-
-            foreach (var user in users)
-            {
-                var existingUser = task.Users.SingleOrDefault(u => u.UserId == user.UserId);
-                if (existingUser == null)
-                {
-                    user.IsOwner = false;
-                    user.TaskId = id;
-                    task.Users.Add(user);
-                }
-                else
-                {
-                    if (existingUser.IsOwner)
-                    {
-                        continue;
-                    }
-                    //todo: add rights check
-                    existingUser.Deadline = user.Deadline;
-                    existingUser.CanRead = user.CanRead;
-                    existingUser.CanWrite = user.CanWrite;
-                    existingUser.CanChangeAccess = user.CanChangeAccess;
-                }
-            }
-            await _context.SaveChangesAsync();
-            return Ok();
         }
 
         [HttpPost("{id}/languages")]
