1bcd2fde7818238480b7194b108f346d298374bd
Andrii Vasylyk <deadsith@hotmail.com>
2020-06-14 21:33:28 +0300

    Add tests for TestingRunner

---
+++ test/auto/Lightest.Tests/TestingService/TestingRunner/BaseTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using Lightest.CodeManagment.Models;
+using Lightest.Data;
+using Lightest.Data.CodeManagment.Services;
+using Lightest.Data.Models.TaskModels;
+using Lightest.TestingService.Interfaces;
+using Lightest.TestingService.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Lightest.Tests.TestingService.TestingRunner
+{
+    public abstract class BaseTest : TestingService.BaseTest
+    {
+        protected Lightest.TestingService.DefaultServices.TestingRunner _testingRunner =>
+            new Lightest.TestingService.DefaultServices.TestingRunner(_scopeFactory.Object,
+                _transferServiceFactory.Object, _uploadProcessorFactory.Object, _logger.Object);
+
+        protected readonly Mock<IServiceScopeFactory> _scopeFactory;
+        protected readonly Mock<IServiceScope> _scope;
+        protected readonly Mock<IServiceProvider> _serviceProvider;
+
+        protected readonly Mock<ITransferServiceFactory> _transferServiceFactory;
+        protected readonly Mock<ITransferService> _transferService;
+
+        protected readonly Mock<IUploadProcessorFactory> _uploadProcessorFactory;
+        protected readonly Mock<IUploadProcessor> _uploadProcessor;
+
+        protected readonly Mock<ILogger<Lightest.TestingService.DefaultServices.TestingRunner>> _logger;
+        protected readonly Mock<ICodeManagmentService> _codeManagmentService;
+
+        public BaseTest()
+        {
+            _codeManagmentService = new Mock<ICodeManagmentService>();
+            _logger = new Mock<ILogger<Lightest.TestingService.DefaultServices.TestingRunner>>();
+
+            _uploadProcessor = new Mock<IUploadProcessor>();
+            _uploadProcessorFactory = new Mock<IUploadProcessorFactory>();
+            _uploadProcessorFactory.Setup(f => f.Create(It.IsAny<Upload>(), It.IsAny<UploadData>(),
+                It.IsAny<TestingServer>(), _context))
+                .Returns(_uploadProcessor.Object);
+
+            _transferService = new Mock<ITransferService>();
+            _transferServiceFactory = new Mock<ITransferServiceFactory>();
+            _transferServiceFactory.Setup(f => f.Create(It.IsAny<IPAddress>(), It.IsAny<int>()))
+                .Returns(_transferService.Object);
+
+            _serviceProvider = new Mock<IServiceProvider>();
+            _serviceProvider.Setup(p => p.GetService(typeof(RelationalDbContext)))
+                .Returns(_context);
+            _serviceProvider.Setup(p => p.GetService(typeof(ICodeManagmentService)))
+                .Returns(_codeManagmentService.Object);
+
+            _scope = new Mock<IServiceScope>();
+            _scope.Setup(s => s.ServiceProvider)
+                .Returns(_serviceProvider.Object);
+
+            _scopeFactory = new Mock<IServiceScopeFactory>();
+            _scopeFactory.Setup(f => f.CreateScope())
+                .Returns(_scope.Object);
+        }
+
+    }
+}
---
+++ test/auto/Lightest.Tests/TestingService/TestingRunner/TryStartNewWorker.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Lightest.Data.Models;
+using Lightest.Data.Models.TaskModels;
+using Lightest.TestingService.Models;
+using Lightest.Tests.Api.Tests.UploadsController;
+using Moq;
+using Xunit;
+
+namespace Lightest.Tests.TestingService.TestingRunner
+{
+    public class TryStartNewWorker : BaseTest
+    {
+        private readonly TestingServerInfo _freeServerInfo;
+        private readonly Upload _queuedUpload;
+
+        private const string UploadCode = nameof(UploadCode);
+
+        public TryStartNewWorker()
+        {
+            _freeServerInfo = new TestingServerInfo
+            {
+                Ip = "1.1.1.1",
+                Port = 443,
+                Status = ServerStatus.Free
+            };
+
+            _queuedUpload = new Upload
+            {
+                Id = Guid.NewGuid(),
+                Status = UploadStatus.Queue
+            };
+        }
+
+        protected async Task AddToDb()
+        {
+            _context.Uploads.Add(new Upload
+            {
+                Id = Guid.NewGuid(),
+                Status = UploadStatus.New
+            });
+            _context.Uploads.Add(new Upload
+            {
+                Id = Guid.NewGuid(),
+                Status = UploadStatus.Failed
+            });
+            _context.Uploads.Add(new Upload
+            {
+                Id = Guid.NewGuid(),
+                Status = UploadStatus.Testing
+            });
+            _context.Uploads.Add(new Upload
+            {
+                Id = Guid.NewGuid(),
+                Status = UploadStatus.EnvironmentSetup
+            });
+            _context.Uploads.Add(_queuedUpload);
+
+            _context.Servers.Add(new TestingServerInfo
+            {
+                Ip = "1.1.1.2",
+                Status = ServerStatus.Busy
+            });
+            _context.Servers.Add(new TestingServerInfo
+            {
+                Ip = "1.1.1.3",
+                Status = ServerStatus.NotResponding
+            });
+            _context.Servers.Add(_freeServerInfo);
+
+            await _context.SaveChangesAsync();
+        }
+
+        [Fact]
+        public async Task NoFreeServer()
+        {
+            _freeServerInfo.Status = ServerStatus.Busy;
+            await AddToDb();
+
+            await _testingRunner.TryStartNewWorker();
+
+            _transferServiceFactory.Verify(f => f.Create(It.IsAny<IPAddress>(), It.IsAny<int>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task NoQueuedUpload()
+        {
+            _queuedUpload.Status = UploadStatus.Failed;
+            await AddToDb();
+
+            await _testingRunner.TryStartNewWorker();
+
+            _codeManagmentService.Verify(c => c.Get(It.IsAny<Guid>()), Times.Never);
+            var server = _context.Servers.First(s => s.Ip == _freeServerInfo.Ip);
+            Assert.Equal(ServerStatus.Free, server.Status);
+        }
+
+        [Fact]
+        public async Task MultipleItemsProcessed()
+        {
+            var uploads = new[]
+            {
+                _queuedUpload,
+                new Upload
+                {
+                    Id = Guid.NewGuid(),
+                    Status = UploadStatus.Queue
+                },
+                new Upload
+                {
+                    Id = Guid.NewGuid(),
+                    Status = UploadStatus.Queue
+                }
+            };
+
+            var data = new CodeManagment.Models.UploadData[3];
+
+            _context.Uploads.Add(uploads[1]);
+            _context.Uploads.Add(uploads[2]);
+
+            for(var i = 0; i < uploads.Length; i++)
+            {
+                var upload = uploads[i];
+                var currentData = new CodeManagment.Models.UploadData
+                {
+                    Id = upload.Id,
+                    Code = $"{UploadCode}{i}"
+                };
+                data[i] = currentData;
+                _codeManagmentService.Setup(cm => cm.Get(upload.Id))
+                    .Returns(currentData);
+            }
+
+            await AddToDb();
+
+            await _testingRunner.TryStartNewWorker();
+
+            for (var i = 0; i < uploads.Length; i++)
+            {
+                var upload = _context.Uploads.First(u => u.Id == uploads[i].Id);
+                var currentData = data[i];
+
+                Assert.Equal(UploadStatus.EnvironmentSetup, upload.Status);
+                _uploadProcessorFactory.Verify(f => f.Create(
+                    It.Is<Upload>(u => u.Id == upload.Id),
+                    currentData,
+                    It.Is<TestingServer>(s => s.TransferService == _transferService.Object
+                        && s.ServerInfo.Ip == _freeServerInfo.Ip),
+                    _context), Times.Once);
+            }
+
+            _uploadProcessor.Verify(p => p.Process(), Times.Exactly(uploads.Length));
+            var server = _context.Servers.First(s => s.Ip == _freeServerInfo.Ip);
+            Assert.Equal(ServerStatus.Free, server.Status);
+        }
+    }
+}
