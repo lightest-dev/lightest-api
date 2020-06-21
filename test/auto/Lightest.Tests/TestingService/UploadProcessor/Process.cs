2d2a8ac03989d33c5250b837d8be33a0c84d5dcd
Andrii Vasylyk <deadsith@hotmail.com>
2020-06-21 18:24:40 +0300

    Add tests for UploadProcessor

---
+++ test/auto/Lightest.Tests/TestingService/UploadProcessor/Process.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using GrpcServices;
+using Lightest.Data.Models.TaskModels;
+using Moq;
+using Xunit;
+
+namespace Lightest.Tests.TestingService.UploadProcessor
+{
+    public class Process : BaseTest
+    {
+        protected override Lightest.TestingService.DefaultServices.UploadProcessor _uploadProcessor =>
+            _processorMock.Object;
+
+        private readonly Mock<Lightest.TestingService.DefaultServices.UploadProcessor> _processorMock;
+        private readonly TestingResponse _testingResponse;
+
+        public Process()
+        {
+            _testingResponse = new TestingResponse
+            {
+                UploadId = _upload.Id.ToString()
+            };
+
+            _processorMock =
+                new Mock<Lightest.TestingService.DefaultServices.UploadProcessor>(_upload, _uploadData, _server, _context);
+
+            _processorMock.Setup(p => p.CacheChecker()).ThrowsAsync(new Exception());
+
+            _processorMock.Setup(p => p.SendData()).ReturnsAsync(_testingResponse);
+
+            _processorMock.Setup(p => p.ReportCodeResult(It.IsAny<TestingResponse>()))
+                .ThrowsAsync(new Exception());
+            _processorMock.Setup(p => p.ReportCodeResult(_testingResponse)).Returns(Task.CompletedTask);
+        }
+
+        [Fact]
+        public async Task CheckerCompilationFailed()
+        {
+            _processorMock.Setup(p => p.CacheChecker()).ReturnsAsync(false);
+            await SetUpData();
+
+            await _uploadProcessor.Process();
+
+            var upload = _context.Uploads.First(u => u.Id == _upload.Id);
+            Assert.Equal(UploadStatus.Failed, upload.Status);
+            Assert.Equal("Checker compilation failed", upload.Message);
+
+            _processorMock.Verify(p => p.SendData(), Times.Never);
+            _processorMock.Verify(p => p.ReportCodeResult(It.IsAny<TestingResponse>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task CheckerCompilationSucceeded()
+        {
+            _processorMock.Setup(p => p.CacheChecker()).ReturnsAsync(true);
+            await SetUpData();
+
+            await _uploadProcessor.Process();
+
+            _processorMock.Verify(p => p.SendData(), Times.Once);
+            _processorMock.Verify(p => p.ReportCodeResult(_testingResponse), Times.Once);
+        }
+    }
+}
---
+++ test/auto/Lightest.Tests/TestingService/UploadProcessor/ReportCodeResult.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrpcServices;
+using Lightest.Data.Models;
+using Xunit;
+
+namespace Lightest.Tests.TestingService.UploadProcessor
+{
+    public class ReportCodeResult : BaseTest
+    {
+        private readonly TestingResponse _result;
+        private readonly Assignment _userTask;
+
+        public ReportCodeResult()
+        {
+            _userTask = new Assignment
+            {
+                UserId = _upload.UserId,
+                TaskId = _task.Id,
+            };
+
+            _task.Tests = new List<Data.Models.TaskModels.Test>
+            {
+                new Data.Models.TaskModels.Test
+                {
+                    Id = Guid.NewGuid()
+                },
+                new Data.Models.TaskModels.Test
+                {
+                    Id = Guid.NewGuid()
+                }
+            };
+
+            _result = new TestingResponse
+            {
+                UploadId = _upload.Id.ToString(),
+                Status = "ResultStatus",
+                Message = "ResultMessage",
+                SuccessfulTestsCount = 1,
+                FailedTestsCount = 1,
+                TestingFailed = false
+            };
+        }
+
+        protected override Task SetUpData()
+        {
+            _context.UserTasks.Add(_userTask);
+
+            return base.SetUpData();
+        }
+
+        [Theory]
+        [InlineData(10, 50)]
+        [InlineData(60, 60)]
+        public async Task NotCompleted(int highScore, int expectedScore)
+        {
+            _userTask.HighScore = highScore;
+            await SetUpData();
+
+            await _uploadProcessor.ReportCodeResult(_result);
+
+            var upload = _context.Uploads.First(u => u.Id == _upload.Id);
+
+            Assert.Equal(50, upload.Points);
+            Assert.Equal(_result.Status, upload.Status);
+            Assert.Equal(_result.Message, upload.Message);
+
+            var asignment = _context.UserTasks.First(a => a.UserId == _upload.UserId &&
+                a.TaskId == _task.Id);
+            Assert.Equal(expectedScore, asignment.HighScore);
+            Assert.False(asignment.Completed);
+        }
+
+        [Fact]
+        public async Task Completed()
+        {
+            _result.SuccessfulTestsCount = 2;
+            _result.FailedTestsCount = 0;
+            await SetUpData();
+
+            await _uploadProcessor.ReportCodeResult(_result);
+
+            var upload = _context.Uploads.First(u => u.Id == _upload.Id);
+
+            Assert.Equal(100, upload.Points);
+            Assert.Equal(_result.Status, upload.Status);
+            Assert.Equal(_result.Message, upload.Message);
+
+            var asignment = _context.UserTasks.First(a => a.UserId == _upload.UserId &&
+                a.TaskId == _task.Id);
+            Assert.Equal(100, asignment.HighScore);
+            Assert.True(asignment.Completed);
+        }
+    }
+}
